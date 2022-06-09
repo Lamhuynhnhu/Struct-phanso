b35b93487563852fa36be5e9325d254149905a17
Lamhuynhnhu <103155958+Lamhuynhnhu@users.noreply.github.com>
2022-06-09 09:26:16 +0700

    Add files via upload

---
+++ FILE.cpp
@@ -0,0 +1,44 @@
+#include<stdio.h>
+ 
+ int main()
+ {
+ 	FILE*fptr;
+ 	// mo file 
+ 	fptr = fopen ("D:\\Lam Huynh Nhu 110121263 Da21ttc\\Array.txt ","r");
+ 	if (fptr== NULL)
+ 	{
+ 	printf(" Loi file");
+ 	return 0;	
+	}
+	// co the thay 
+ 	/*char hoten[50];
+ 	printf ("Ho va ten ");
+ 	gets(hoten);
+ 	fprintf(fptr,"%s\n",hoten);
+ 	fflush(stdin);
+ 	int tuoi;
+ 	printf ("Tuoi");
+ 	scanf("%d",&tuoi);
+ 	fprintf(fptr,"%d",tuoi);*/
+ 	
+ 	                           // doc file tung cai
+ 	int n;
+ 	fscanf(fptr,"%d",&n);
+	printf("%d\n",n);
+ 	
+ 	int x;
+ 	for (int i=1; i<=n; i++)
+ 	{
+ 		fscanf(fptr, "%d",&x);
+ 		printf("%d\n",x);
+	 }
+                                // doc toan bo file
+//  int t;
+//  while (fscanf(fptr,"%d",&t)!=EOF)
+//  printf("%d\n",t);                              
+//                             
+ 	// dong file 
+ 	fclose (fptr);
+ 	return 0;
+ 	
+ }
